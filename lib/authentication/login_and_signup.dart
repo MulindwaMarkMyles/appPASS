49aee5b7c106d91c69d6195f6f7b31aebf785dbc
Kiggundu Sulaiman <kiggundusulaiman332@gmail.com>
2024-07-15 08:19:46 +0300

    android icon

--- lib/authentication/login_and_signup.dart
+++ lib/authentication/login_and_signup.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sort_child_properties_last
+
 import 'package:app_pass/authentication/signup.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
