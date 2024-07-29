e8e6206f0a956fb50e0d685b240a57c5940c1c3a
prayzzoey <ankunda.praise@students.mak.ac.ug>
2024-07-29 11:53:23 +0300

    homepage fixes

--- lib/actions/biometric_stub.dart
+++
@@ -1,7 +0,0 @@
-import 'package:app_pass/actions/biometric_mobile.dart';
-
-Future<bool> isAuthenticated() async {
-  bool authenticated = false;
-  authenticated = await authenticate();
-  return authenticated;
-  }
