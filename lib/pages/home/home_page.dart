94ec7c5f1ef0c2909455f0c507bfb53b886ba06c
prayzzoey <ankunda.praise@students.mak.ac.ug>
2024-07-30 15:39:16 +0300

    homescreen fixes

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -187,6 +187,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Row(
           children: [
             Image.asset(
