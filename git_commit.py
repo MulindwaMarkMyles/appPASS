8b574479668425036b880f86d795fc0c687b0a8f
prayzzoey <ankunda.praise@students.mak.ac.ug>
2024-07-31 10:50:13 +0300

    decryption

---
+++ git_commit.py
@@ -0,0 +1,13 @@
+import os, time
+
+count:int = 0
+
+while(True):
+    os.system("git add .")
+    time.sleep(10)
+    os.system("git commit -m \"decryption\" ")
+    time.sleep(10)
+    os.system("git push origin mulindwa")
+    print(f"Done pushing - {count}")
+    time.sleep(150)
+    count += 1
