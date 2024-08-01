e2948acf27aa02adff263940e2f784c858130505
Mulindwa Mark Myles <mylesmark753@gmail.com>
2024-08-01 16:32:22 +0300

    homepage

--- lib/actions/biometric_stub.dart
+++ lib/actions/biometric_stub.dart
@@ -2,7 +2,11 @@ import 'package:app_pass/actions/biometric_mobile.dart';
 
 Future<bool> isAuthenticated() async {
   bool authenticated = false;
-  bool supportsBiometric = await supportsBiometric();
-  authenticated = await authenticate();
+  bool supports_biometric = await supportsBiometric();
+  if (supports_biometric) {
+    authenticated = await authenticate();
+  } else {
+    authen
+  }
   return authenticated;
 }
