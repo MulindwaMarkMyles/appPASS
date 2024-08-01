a1ff13de64b6d5488ba95a7ad351ef761a618f79
Kiggundu Sulaiman <kiggundusulaiman332@gmail.com>
2024-08-01 15:20:50 +0300

    homepage

--- lib/pages/home/wifi.dart
+++ lib/pages/home/wifi.dart
@@ -4,6 +4,7 @@ import 'package:ionicons/ionicons.dart';
 import 'PasswordDetailsPage.dart';
 import 'package:app_pass/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class Wifi extends StatefulWidget {
   const Wifi({Key? key}) : super(key: key);
@@ -65,7 +66,8 @@ class WifiState extends State<Wifi> {
         future: _passwordsFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: LoadingAnimationWidget.threeRotatingDots(
+            return Center(
+                child: LoadingAnimationWidget.threeRotatingDots(
                     color: Color.fromARGB(255, 243, 134, 84), size: 50));
           } else if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
