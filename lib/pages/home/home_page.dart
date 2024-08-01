2189cb15cebc341c89ed007b2a0aa15bca9863ed
Mulindwa Mark Myles <mylesmark753@gmail.com>
2024-08-01 13:11:01 +0300

    homepage

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -710,21 +710,3 @@ class AddPasswordPageState extends State<AddPasswordPage> {
   }
 }
 
-class AddPasswordScreen extends StatelessWidget {
-  final Function onPasswordAdded;
-
-  const AddPasswordScreen({required this.onPasswordAdded});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Add Password'),
-        backgroundColor: Color.fromRGBO(246, 208, 183, 1),
-      ),
-      body: Center(
-        child: Text('Add Password Screen'),
-      ),
-    );
-  }
-}
